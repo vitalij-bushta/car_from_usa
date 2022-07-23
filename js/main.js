8ba3d834995d609d71c3f5d2a570c553074876d3
Vitalii <vitalij.bushta@gmail.com>
2022-07-23 11:54:39 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.carusel').slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+   
+        });
+        
+});
